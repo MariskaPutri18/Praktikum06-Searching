7ed773f385b18f7e5f20627b501fa4499c2347fd
MariskaPutri18 <mariskaputri960@gmail.com>
2024-03-27 15:01:46 +0700

    mergesort

--- Mergesorting18.java
+++ Mergesorting18.java
@@ -1,7 +1,4 @@
 public class Mergesorting18 {
-
-  
-
    private void merge(int data[], int left, int middle, int right){
         int [] temp = new int [data.length];
         for (int i = left; i <= right; i ++){
@@ -25,9 +22,7 @@ public class Mergesorting18 {
         for (int i = 0; i <=s; i++){
             data[c +i] = temp [a+i];
         }
-    
     }
-
     private void sort (int data[], int left, int right){
         if (left<right){
             int middle = (left + right) /2;
@@ -37,12 +32,9 @@ public class Mergesorting18 {
       
         }
     }
-
         public void mergesort (int data[]){
         this.sort(data, 0, data.length - 1);    
     }
-    
-
     public void printarray(int arr[]){
         int n =arr.length;
         for (int i =0; i<n; i++){
